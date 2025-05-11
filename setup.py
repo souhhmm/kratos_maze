586e94b53ae12dd7d615d35c2587ae18e04bc6d4
souhhmm <kalburgisoham@gmail.com>
2025-05-11 18:54:29 +0530

    setup maze

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
         (os.path.join('share', package_name, 'launch'), glob('launch/*')),
         (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
         (os.path.join('share', package_name, 'models/sample'), glob('models/sample/*')),
+        (os.path.join('share', package_name, 'models/maze'), glob('models/maze/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
